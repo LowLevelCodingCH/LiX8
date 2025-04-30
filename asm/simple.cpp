d40a14593bf74b6e4ae5e60732344b9ac4110585
LowLevelCodingCH <llcch@proton.me>
2025-04-30 16:36:55 +0200

    ok formatted okokookookokoo

--- asm/simple.cpp
+++ asm/simple.cpp
@@ -1,12 +1,13 @@
-#include <vector>
-#include <string>
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-enum reg {
+enum reg
+{
 	PC, // program counter
 	SP, // stack pointer
 
@@ -25,7 +26,8 @@ enum reg {
 	F1,
 };
 
-enum inst {
+enum inst
+{
 	NOP,
 	CPY,
 	MOV,
@@ -68,11 +70,14 @@ std::vector<std::string> split(std::string string, char delimiter)
 	std::string buffer;
 	std::vector<std::string> result;
 
-	for(char c : string) {
-		if(c == delimiter) {
+	for (char c : string)
+	{
+		if (c == delimiter)
+		{
 			result.push_back(buffer);
 			buffer.clear();
-		} else
+		}
+		else
 			buffer.push_back(c);
 	}
 
@@ -82,106 +87,111 @@ std::vector<std::string> split(std::string string, char delimiter)
 
 int main(int argc, char *argv[])
 {
-	if(argc != 2)
+	if (argc != 2)
 		exit(1);
 
-	std::ifstream       file(argv[1]);
-	std::string         line;
-	std::stringstream   text;
-	std::vector<short>  output;
+	std::ifstream file(argv[1]);
+	std::string line;
+	std::stringstream text;
+	std::vector<short> output;
 
-	while(std::getline(file, line))
+	while (std::getline(file, line))
 		text << line << ' ';
 
 	file.close();
 
 	auto htoks = split(text.str(), ' ');
 
-	for(auto htok : htoks) {
-		if("nop" == htok)
-			output.push_back((short)inst::NOP);
-		else if("cpy" == htok)
-			output.push_back((short)inst::CPY);
-		else if("mov" == htok)
-			output.push_back((short)inst::MOV);
-		else if("movi" == htok)
-			output.push_back((short)inst::MOVI);
-		else if("lod" == htok)
-			output.push_back((short)inst::LOD);
-		else if("inc" == htok)
-			output.push_back((short)inst::INC);
-		else if("dec" == htok)
-			output.push_back((short)inst::DEC);
-		else if("add" == htok)
-			output.push_back((short)inst::ADD);
-		else if("sub" == htok)
-			output.push_back((short)inst::SUB);
-		else if("mul" == htok)
-			output.push_back((short)inst::MUL);
-		else if("div" == htok)
-			output.push_back((short)inst::DIV);
-		else if("push" == htok)
-			output.push_back((short)inst::PUSH);
-		else if("pop" == htok)
-			output.push_back((short)inst::POP);
-		else if("ret" == htok)
-			output.push_back((short)inst::RET);
-		else if("int" == htok)
-			output.push_back((short)inst::INT);
-		else if("reti" == htok)
-			output.push_back((short)inst::RETI);
-		else if("call" == htok)
-			output.push_back((short)inst::CALL);
-		else if("rcall" == htok)
-			output.push_back((short)inst::RCALL);
-		else if("cmp" == htok)
-			output.push_back((short)inst::CMP);
-		else if("jmp" == htok)
-			output.push_back((short)inst::JMP);
-		else if("jz" == htok)
-			output.push_back((short)inst::JZ);
-		else if("jl" == htok)
-			output.push_back((short)inst::JL);
-		else if("jm" == htok)
-			output.push_back((short)inst::JM);
-		else if("jne" == htok)
-			output.push_back((short)inst::JNE);
-		else if("hlt" == htok)
-			output.push_back((short)inst::HLT);
-		else if("out" == htok)
-			output.push_back((short)inst::OUT);
-		else if("in" == htok)
-			output.push_back((short)inst::IN);
-		else if("sp" == htok)
-			output.push_back((short)reg::SP);
-		else if("l0" == htok || "r0" == htok)
-			output.push_back((short)reg::L0);
-		else if("l1" == htok || "r1" == htok)
-			output.push_back((short)reg::L1);
-		else if("l2" == htok || "r2" == htok)
-			output.push_back((short)reg::L2);
-		else if("l3" == htok || "r3" == htok)
-			output.push_back((short)reg::L3);
-		else if("l4" == htok || "r4" == htok)
-			output.push_back((short)reg::L4);
-		else if("l5" == htok || "r5" == htok)
-			output.push_back((short)reg::L5);
-		else if("l6" == htok || "r6" == htok)
-			output.push_back((short)reg::L6);
-		else if("l7" == htok || "r7" == htok)
-			output.push_back((short)reg::L7);
-		else if("i0" == htok)
-			output.push_back((short)reg::I0);
-		else {
-			try {
-				output.push_back((short)std::stoi(htok));
-			} catch (...) {
-				output.push_back((short)0);
+	for (auto htok : htoks)
+	{
+		if ("nop" == htok)
+			output.push_back((short) inst::NOP);
+		else if ("cpy" == htok)
+			output.push_back((short) inst::CPY);
+		else if ("mov" == htok)
+			output.push_back((short) inst::MOV);
+		else if ("movi" == htok)
+			output.push_back((short) inst::MOVI);
+		else if ("lod" == htok)
+			output.push_back((short) inst::LOD);
+		else if ("inc" == htok)
+			output.push_back((short) inst::INC);
+		else if ("dec" == htok)
+			output.push_back((short) inst::DEC);
+		else if ("add" == htok)
+			output.push_back((short) inst::ADD);
+		else if ("sub" == htok)
+			output.push_back((short) inst::SUB);
+		else if ("mul" == htok)
+			output.push_back((short) inst::MUL);
+		else if ("div" == htok)
+			output.push_back((short) inst::DIV);
+		else if ("push" == htok)
+			output.push_back((short) inst::PUSH);
+		else if ("pop" == htok)
+			output.push_back((short) inst::POP);
+		else if ("ret" == htok)
+			output.push_back((short) inst::RET);
+		else if ("int" == htok)
+			output.push_back((short) inst::INT);
+		else if ("reti" == htok)
+			output.push_back((short) inst::RETI);
+		else if ("call" == htok)
+			output.push_back((short) inst::CALL);
+		else if ("rcall" == htok)
+			output.push_back((short) inst::RCALL);
+		else if ("cmp" == htok)
+			output.push_back((short) inst::CMP);
+		else if ("jmp" == htok)
+			output.push_back((short) inst::JMP);
+		else if ("jz" == htok)
+			output.push_back((short) inst::JZ);
+		else if ("jl" == htok)
+			output.push_back((short) inst::JL);
+		else if ("jm" == htok)
+			output.push_back((short) inst::JM);
+		else if ("jne" == htok)
+			output.push_back((short) inst::JNE);
+		else if ("hlt" == htok)
+			output.push_back((short) inst::HLT);
+		else if ("out" == htok)
+			output.push_back((short) inst::OUT);
+		else if ("in" == htok)
+			output.push_back((short) inst::IN);
+		else if ("sp" == htok)
+			output.push_back((short) reg::SP);
+		else if ("l0" == htok || "r0" == htok)
+			output.push_back((short) reg::L0);
+		else if ("l1" == htok || "r1" == htok)
+			output.push_back((short) reg::L1);
+		else if ("l2" == htok || "r2" == htok)
+			output.push_back((short) reg::L2);
+		else if ("l3" == htok || "r3" == htok)
+			output.push_back((short) reg::L3);
+		else if ("l4" == htok || "r4" == htok)
+			output.push_back((short) reg::L4);
+		else if ("l5" == htok || "r5" == htok)
+			output.push_back((short) reg::L5);
+		else if ("l6" == htok || "r6" == htok)
+			output.push_back((short) reg::L6);
+		else if ("l7" == htok || "r7" == htok)
+			output.push_back((short) reg::L7);
+		else if ("i0" == htok)
+			output.push_back((short) reg::I0);
+		else
+		{
+			try
+			{
+				output.push_back((short) std::stoi(htok));
+			}
+			catch (...)
+			{
+				output.push_back((short) 0);
 			}
 		}
 	}
 
 	std::ofstream ofile("a.bin");
-	ofile.write(reinterpret_cast<char*>(output.data()), output.size() * sizeof(short));
+	ofile.write(reinterpret_cast<char *>(output.data()), output.size() * sizeof(short));
 	ofile.close();
 }
--- main.cpp
+++ main.cpp
@@ -1,74 +1,79 @@
-#include <iostream>
+#include <cmath>
 #include <cstdint>
 #include <fstream>
+#include <iostream>
 #include <sstream>
-#include <cmath>
 
 #define PROGLEN 12
 #define PROGADR 0
 
-enum reg {
-    PC, // program counter
-    SP, // stack pointer
+enum reg
+{
+	PC, // program counter
+	SP, // stack pointer
 
-    L0,
-    L1,
-    L2,
-    L3,
-    L4,
-    L5,
-    L6,
-    L7,
+	L0,
+	L1,
+	L2,
+	L3,
+	L4,
+	L5,
+	L6,
+	L7,
 
-    I0, // interrupt vector
+	I0, // interrupt vector
 
-    F0,
-    F1,
+	F0,
+	F1,
 };
 
-enum inst {
-    NOP,
-    CPY,
-    MOV,
-    LOD,
-    MOVI,
-    INC,
-    DEC,
+enum inst
+{
+	NOP,
+	CPY,
+	MOV,
+	LOD,
+	MOVI,
+	INC,
+	DEC,
 
-    ADD,
-    SUB,
-    MUL,
-    DIV,
+	ADD,
+	SUB,
+	MUL,
+	DIV,
 
-    PUSH,
-    POP,
+	PUSH,
+	POP,
 
-    CALL,
-    RCALL,
-    RET,
+	CALL,
+	RCALL,
+	RET,
 
-    CMP,
+	CMP,
 
-    JMP,
-    JZ,
-    JL,
-    JM,
-    JNE,
+	JMP,
+	JZ,
+	JL,
+	JM,
+	JNE,
 
-    HLT,
+	HLT,
 
-    INT,
-    RETI,
+	INT,
+	RETI,
 
-    OUT,
-    IN,
+	OUT,
+	IN,
 };
 
-#define cr(I) case I : return #I
+#define cr(I)                                                                                         \
+	case I:                                                                                       \
+		return #I
 
 std::string intoa(char i)
 {
-	switch(i) {
+	switch (i)
+	{
 		cr(NOP);
 		cr(INT);
 		cr(CPY);
@@ -94,120 +99,193 @@ std::string intoa(char i)
 		cr(HLT);
 		cr(OUT);
 		cr(IN);
-		default: return "UNKNOWN";
+		default:
+			return "UNKNOWN";
 	}
 }
 
-struct lix {
-    std::uint16_t registers[13];
-    std::uint16_t inst;
-    std::uint16_t arg0;
-    std::uint16_t arg1;
-    std::uint8_t rmemory[16384];  // Not included by def
-    std::uint16_t *memory;
+struct lix
+{
+	std::uint16_t registers[13];
+	std::uint16_t inst;
+	std::uint16_t arg0;
+	std::uint16_t arg1;
+	std::uint8_t rmemory[16384]; // Not included by def
+	std::uint16_t *memory;
 
-    void printprog() {
-        for(int i = 0; i < PROGLEN*3; i++)
-            std::cout << (int)(((char*)(this->memory))[PROGADR+i]) << " ";
-        std::cout << std::endl;
-    }
-
-    void printinst() {
-        std::cout << intoa(this->inst) << ", " << (int)this->arg0 << ", " << (int)this->arg1 << std::endl;
-    }
-
-    void fetch() {
-        this->inst = this->memory[PROGADR+this->registers[reg::PC]];
-        this->arg0 = this->memory[PROGADR+this->registers[reg::PC]+1];
-        this->arg1 = this->memory[PROGADR+this->registers[reg::PC]+2];
-        this->registers[reg::PC]+=3;
-    }
-
-    void execute() {
-        switch(this->inst) {
-            case inst::CMP: {
-                if(this->registers[(reg)this->arg0]  >  this->registers[(reg)this->arg1])
-                        this->registers[reg::F1] = 1;
-                if(this->registers[(reg)this->arg0]  <  this->registers[(reg)this->arg1])
-                        this->registers[reg::F1] = 2;
-                if(this->registers[(reg)this->arg0]  == this->registers[(reg)this->arg1])
-                        this->registers[reg::F1] = 0;
-            };                                                                                                                                                       break; // break out
-            case inst::NOP:                                                                                                                                   break; // break out
-	    case inst::HLT: exit(0);
-	    case inst::PUSH: this->memory[this->registers[reg::SP]] = this->registers[(reg)this->arg0]; this->registers[reg::SP]++; break;
-	    case inst::POP: this->registers[(reg)this->arg0] = this->memory[this->registers[reg::SP]]; this->registers[reg::SP]--; break;
-	    case inst::RCALL:
-			    this->memory[this->registers[reg::SP]] = this->registers[reg::PC];
-			    this->registers[reg::SP]++;
-			    this->registers[reg::PC] = this->registers[(reg)this->arg0];
-			    break;
-	    case inst::CALL:
-			    this->memory[this->registers[reg::SP]] = this->registers[reg::PC];
-			    this->registers[reg::SP]++;
-			    this->registers[reg::PC] = this->arg0;
-			    break;
-	    case inst::RET:
-			    this->registers[reg::PC] = this->memory[this->registers[reg::SP] - 1];
-			    break;
-            case inst::CPY: this->registers[(reg)this->arg0]                           = this->registers[(reg)this->arg1];                                    break; // break out
-            case inst::JMP: this->registers[reg::PC]                                   = this->arg0;                                                          break; // break out
-            case inst::JNE: if(this->registers[reg::F1] != 0) this->registers[reg::PC] = this->arg0;                                                          break; // break out
-            case inst::INT:
-	    		    this->memory[this->registers[reg::SP]] = this->registers[reg::PC];
-			    this->registers[reg::SP]++;
-			    this->memory[this->registers[reg::SP]] = this->registers[reg::F0];
-			    this->registers[reg::SP]++;
-			    this->memory[this->registers[reg::SP]] = this->registers[reg::F1];
-			    this->registers[reg::SP]++;
-			    this->registers[reg::PC] = this->memory[this->registers[reg::I0] + this->arg0];
-			    std::cout << "PC: " << this->registers[reg::PC] << " F0: " << this->registers[reg::F0] << " F1: " << this->registers[reg::F1] << std::endl;
-			    break;
-	    case inst::RETI:
-			    this->registers[reg::F1] = this->memory[this->registers[reg::SP] - 1];
-			    this->registers[reg::F0] = this->memory[this->registers[reg::SP] - 2];
-			    this->registers[reg::PC] = this->memory[this->registers[reg::SP] - 3];
-			    this->registers[reg::SP] -= 3;
-			    std::cout << "PC: " << this->registers[reg::PC] << " F0: " << this->registers[reg::F0] << " F1: " << this->registers[reg::F1] << std::endl;
-			    break;
-	    case inst::JZ:  if(this->registers[reg::F1] == 0) this->registers[reg::PC] = this->arg0;                                                          break; // break out
-            case inst::JM:  if(this->registers[reg::F1] == 1) this->registers[reg::PC] = this->arg0;                                                          break; // break out
-            case inst::JL:  if(this->registers[reg::F1] == 2) this->registers[reg::PC] = this->arg0;                                                          break; // break out
-	    case inst::MOV: this->memory[this->registers[(reg)this->arg0]]             = this->registers[(reg)this->arg1];                                    break; // break out
-	    case inst::LOD: this->registers[(reg)this->arg1] = this->memory[this->registers[(reg)this->arg0]]; break;
-	    case inst::MOVI: this->registers[(reg)this->arg0]             = this->arg1;                                    break; // break out
-            case inst::INC: this->registers[(reg)this->arg0]++; break; // break out
-            case inst::DEC: this->registers[(reg)this->arg0]--; break; // break out
-            case inst::ADD: this->registers[reg::F0]                                   = this->registers[(reg)this->arg1] + this->registers[(reg)this->arg0]; break; // break out
-            case inst::MUL: this->registers[reg::F0]                                   = this->registers[(reg)this->arg1] * this->registers[(reg)this->arg0]; break; // break out
-            case inst::SUB: this->registers[reg::F0]                                   = this->registers[(reg)this->arg1] - this->registers[(reg)this->arg0]; break; // break out
-            case inst::IN:                                                                                                                                    break; // break out
-            case inst::OUT:                                                                                                                                   break; // break out
-            default:                                                                                                                                                 break; // break out
-        }
-    }
-
-    void clearmem() {
-        for(int i = 0; i < 4096; i++)
-            this->memory[i] = 0;
-    }
-
-    void clearreg() {
-        for(int i = 0; i < 13; i++)
-            this->registers[i] = 0;
-    }
-
-    void init() {
-	this->memory = (unsigned short*)&(this->rmemory[0]);
-        this->clearmem();
-        this->clearreg();
-    }
-
-    void load(short *insts, int len) {
-    	for(int i = 0; i < len; i++) {
-		this->memory[PROGADR+i] = insts[i];
+	void printprog()
+	{
+		for (int i = 0; i < PROGLEN * 3; i++)
+			std::cout << (int) (((char *) (this->memory))[PROGADR + i]) << " ";
+		std::cout << std::endl;
 	}
-    }
+
+	void printinst()
+	{
+		std::cout << intoa(this->inst) << ", " << (int) this->arg0 << ", " << (int) this->arg1
+			  << std::endl;
+	}
+
+	void fetch()
+	{
+		this->inst = this->memory[PROGADR + this->registers[reg::PC]];
+		this->arg0 = this->memory[PROGADR + this->registers[reg::PC] + 1];
+		this->arg1 = this->memory[PROGADR + this->registers[reg::PC] + 2];
+		this->registers[reg::PC] += 3;
+	}
+
+	void execute()
+	{
+		switch (this->inst)
+		{
+			case inst::CMP:
+			{
+				if (this->registers[(reg) this->arg0] >
+				    this->registers[(reg) this->arg1])
+					this->registers[reg::F1] = 1;
+				if (this->registers[(reg) this->arg0] <
+				    this->registers[(reg) this->arg1])
+					this->registers[reg::F1] = 2;
+				if (this->registers[(reg) this->arg0] ==
+				    this->registers[(reg) this->arg1])
+					this->registers[reg::F1] = 0;
+			};
+			break; // break out
+			case inst::NOP:
+				break; // break out
+			case inst::HLT:
+				exit(0);
+			case inst::PUSH:
+				this->memory[this->registers[reg::SP]] =
+				    this->registers[(reg) this->arg0];
+				this->registers[reg::SP]++;
+				break;
+			case inst::POP:
+				this->registers[(reg) this->arg0] =
+				    this->memory[this->registers[reg::SP]];
+				this->registers[reg::SP]--;
+				break;
+			case inst::RCALL:
+				this->memory[this->registers[reg::SP]] = this->registers[reg::PC];
+				this->registers[reg::SP]++;
+				this->registers[reg::PC] = this->registers[(reg) this->arg0];
+				break;
+			case inst::CALL:
+				this->memory[this->registers[reg::SP]] = this->registers[reg::PC];
+				this->registers[reg::SP]++;
+				this->registers[reg::PC] = this->arg0;
+				break;
+			case inst::RET:
+				this->registers[reg::PC] = this->memory[this->registers[reg::SP] - 1];
+				break;
+			case inst::CPY:
+				this->registers[(reg) this->arg0] = this->registers[(reg) this->arg1];
+				break; // break out
+			case inst::JMP:
+				this->registers[reg::PC] = this->arg0;
+				break; // break out
+			case inst::JNE:
+				if (this->registers[reg::F1] != 0)
+					this->registers[reg::PC] = this->arg0;
+				break; // break out
+			case inst::INT:
+				this->memory[this->registers[reg::SP]] = this->registers[reg::PC];
+				this->registers[reg::SP]++;
+				this->memory[this->registers[reg::SP]] = this->registers[reg::F0];
+				this->registers[reg::SP]++;
+				this->memory[this->registers[reg::SP]] = this->registers[reg::F1];
+				this->registers[reg::SP]++;
+				this->registers[reg::PC] =
+				    this->memory[this->registers[reg::I0] + this->arg0];
+				std::cout << "PC: " << this->registers[reg::PC]
+					  << " F0: " << this->registers[reg::F0]
+					  << " F1: " << this->registers[reg::F1] << std::endl;
+				break;
+			case inst::RETI:
+				this->registers[reg::F1] = this->memory[this->registers[reg::SP] - 1];
+				this->registers[reg::F0] = this->memory[this->registers[reg::SP] - 2];
+				this->registers[reg::PC] = this->memory[this->registers[reg::SP] - 3];
+				this->registers[reg::SP] -= 3;
+				std::cout << "PC: " << this->registers[reg::PC]
+					  << " F0: " << this->registers[reg::F0]
+					  << " F1: " << this->registers[reg::F1] << std::endl;
+				break;
+			case inst::JZ:
+				if (this->registers[reg::F1] == 0)
+					this->registers[reg::PC] = this->arg0;
+				break; // break out
+			case inst::JM:
+				if (this->registers[reg::F1] == 1)
+					this->registers[reg::PC] = this->arg0;
+				break; // break out
+			case inst::JL:
+				if (this->registers[reg::F1] == 2)
+					this->registers[reg::PC] = this->arg0;
+				break; // break out
+			case inst::MOV:
+				this->memory[this->registers[(reg) this->arg0]] =
+				    this->registers[(reg) this->arg1];
+				break; // break out
+			case inst::LOD:
+				this->registers[(reg) this->arg1] =
+				    this->memory[this->registers[(reg) this->arg0]];
+				break;
+			case inst::MOVI:
+				this->registers[(reg) this->arg0] = this->arg1;
+				break; // break out
+			case inst::INC:
+				this->registers[(reg) this->arg0]++;
+				break; // break out
+			case inst::DEC:
+				this->registers[(reg) this->arg0]--;
+				break; // break out
+			case inst::ADD:
+				this->registers[reg::F0] = this->registers[(reg) this->arg1] +
+							   this->registers[(reg) this->arg0];
+				break; // break out
+			case inst::MUL:
+				this->registers[reg::F0] = this->registers[(reg) this->arg1] *
+							   this->registers[(reg) this->arg0];
+				break; // break out
+			case inst::SUB:
+				this->registers[reg::F0] = this->registers[(reg) this->arg1] -
+							   this->registers[(reg) this->arg0];
+				break; // break out
+			case inst::IN:
+				break; // break out
+			case inst::OUT:
+				break; // break out
+			default:
+				break; // break out
+		}
+	}
+
+	void clearmem()
+	{
+		for (int i = 0; i < 4096; i++)
+			this->memory[i] = 0;
+	}
+
+	void clearreg()
+	{
+		for (int i = 0; i < 13; i++)
+			this->registers[i] = 0;
+	}
+
+	void init()
+	{
+		this->memory = (unsigned short *) &(this->rmemory[0]);
+		this->clearmem();
+		this->clearreg();
+	}
+
+	void load(short *insts, int len)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			this->memory[PROGADR + i] = insts[i];
+		}
+	}
 };
 
 // Li X 16 CPU
@@ -216,61 +294,65 @@ struct lix {
 #undef HW_SUP
 #define SGA_ADR 0x20
 
-int main() {
-    lix cpu;
-    cpu.init();
+int main()
+{
+	lix cpu;
+	cpu.init();
 
-    cpu.memory[PROGADR+0]  = MOVI;     // state: l0 = 3, nothing else changes
-    cpu.memory[PROGADR+1]  = reg::L0;
-    cpu.memory[PROGADR+2]  = 3;
+	cpu.memory[PROGADR + 0] = MOVI; // state: l0 = 3, nothing else changes
+	cpu.memory[PROGADR + 1] = reg::L0;
+	cpu.memory[PROGADR + 2] = 3;
 
-    cpu.memory[PROGADR+3]  = CALL; // state: l0 = 3, mem[pc] = pc + 3 (6)
-    cpu.memory[PROGADR+4]  = 6;
-    cpu.memory[PROGADR+5]  = 0;
+	cpu.memory[PROGADR + 3] = CALL; // state: l0 = 3, mem[pc] = pc + 3 (6)
+	cpu.memory[PROGADR + 4] = 6;
+	cpu.memory[PROGADR + 5] = 0;
 
-    cpu.memory[PROGADR+6]  = ADD; // l0 = 3, l7 = 3 + l1, mem[pc] = 6
-    cpu.memory[PROGADR+7]  = reg::L0;
-    cpu.memory[PROGADR+8]  = reg::L1;
+	cpu.memory[PROGADR + 6] = ADD; // l0 = 3, l7 = 3 + l1, mem[pc] = 6
+	cpu.memory[PROGADR + 7] = reg::L0;
+	cpu.memory[PROGADR + 8] = reg::L1;
 
-    cpu.memory[PROGADR+9]  = RET; // l0 = 3, l7 = 3 + l1, mem[pc] = 0
-    cpu.memory[PROGADR+10] = 0;
-    cpu.memory[PROGADR+11] = 0;
+	cpu.memory[PROGADR + 9] = RET; // l0 = 3, l7 = 3 + l1, mem[pc] = 0
+	cpu.memory[PROGADR + 10] = 0;
+	cpu.memory[PROGADR + 11] = 0;
 
 #define target L1
 #define counter L0
 
 	short prog[101] = {0};
 
-std::ifstream file("a.bin");
-std::ostringstream a;
-a << file.rdbuf();
-int i = 0;
-for(i = 0; i < a.str().size() / 2; i++) {
-	short b = ((short*)(a.str().data()))[i];
-	prog[i] = b;
-}
+	std::ifstream file("a.bin");
+	std::ostringstream a;
+	a << file.rdbuf();
+	int i = 0;
+	for (i = 0; i < a.str().size() / 2; i++)
+	{
+		short b = ((short *) (a.str().data()))[i];
+		prog[i] = b;
+	}
 
-    cpu.load(prog, 30);
-    cpu.printprog();
+	cpu.load(prog, 30);
+	cpu.printprog();
 
-    for(int i = 0; i < PROGLEN; i++) {
-        cpu.fetch();
-	if(cpu.inst == HLT) goto endin;
-        cpu.execute();
-	cpu.printinst();
-	std::cout << "L0: " << (int)cpu.registers[reg::L0] << "\n";
-    std::cout << "L1: " << (int)cpu.registers[reg::L1] << "\n";
-	std::cout << "F0: " << (int)cpu.registers[reg::F0] << "\n";
-	std::cout << "PC: " << (int)cpu.registers[reg::PC] << "\n";
+	for (int i = 0; i < PROGLEN; i++)
+	{
+		cpu.fetch();
+		if (cpu.inst == HLT)
+			goto endin;
+		cpu.execute();
+		cpu.printinst();
+		std::cout << "L0: " << (int) cpu.registers[reg::L0] << "\n";
+		std::cout << "L1: " << (int) cpu.registers[reg::L1] << "\n";
+		std::cout << "F0: " << (int) cpu.registers[reg::F0] << "\n";
+		std::cout << "PC: " << (int) cpu.registers[reg::PC] << "\n";
 
-	std::cout << "-----------------------------------------------\n";
-    }
+		std::cout << "-----------------------------------------------\n";
+	}
 
 endin:
-	std::cout << "L0: " << (int)cpu.registers[reg::L0] << "\n";
-    std::cout << "L1: " << (int)cpu.registers[reg::L1] << "\n";
-	std::cout << "F0: " << (int)cpu.registers[reg::F0] << "\n";
+	std::cout << "L0: " << (int) cpu.registers[reg::L0] << "\n";
+	std::cout << "L1: " << (int) cpu.registers[reg::L1] << "\n";
+	std::cout << "F0: " << (int) cpu.registers[reg::F0] << "\n";
 	std::cout << "-----------------------------------------------\n";
 
-    return 0;
+	return 0;
 }
