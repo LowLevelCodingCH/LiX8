e27c8ae68bd1c4cee6050e8a16ab92e88fd82791
LowLevelCodingCH <llcch@proton.me>
2025-05-05 20:17:12 +0200

    instructions now arent fixed to 3 words

--- os.s
+++ os.s
@@ -7,9 +7,6 @@
 # It is because of my laziness though (not to exclude the : when parsing symbols)
 :
 
-#####################
-###### TODO: remove useless args since emu handles them
-
 # WAIT: 3 things for reading and understanding this code better:
 # 1: read ALL comments. assembly becomes ASSembly otherwise and you wanna cripple
 # 2: read down there where it says "Just so you know variables". trust me, itll help-
